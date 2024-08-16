e6ecefa0ad788f8d60928bb65af10a7a45456981
Juan Roldán <juan.roldan.zamudio@gmail.com>
2024-08-15 18:34:48 -0600

    Agregue un logger al controller

--- clinicaOdontologica/src/main/java/dh/backend/proyectoIntegrador/clinicaOdontologica/controller/PacienteController.java
+++ clinicaOdontologica/src/main/java/dh/backend/proyectoIntegrador/clinicaOdontologica/controller/PacienteController.java
@@ -1,7 +1,10 @@
 package dh.backend.proyectoIntegrador.clinicaOdontologica.controller;
 
+import dh.backend.proyectoIntegrador.clinicaOdontologica.dao.impl.DaoH2Domicilio;
 import dh.backend.proyectoIntegrador.clinicaOdontologica.model.Paciente;
 import dh.backend.proyectoIntegrador.clinicaOdontologica.service.PacienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,8 @@ public class PacienteController {
 
     private PacienteService pacienteService;
 
+    public static final Logger logger = LoggerFactory.getLogger(DaoH2Domicilio.class);
+
     public PacienteController(PacienteService pacienteService) {
         this.pacienteService = pacienteService;
     }
@@ -19,9 +24,11 @@ public class PacienteController {
     @GetMapping("/index")
     public String buscarPaciente(Model model, @RequestParam Integer id){
         Paciente paciente = pacienteService.buscarPorId(id);
+        logger.info("Paciente devuelto al controller: " + paciente);
 
         model.addAttribute("nombre", paciente.getNombre());
         model.addAttribute("apellido", paciente.getApellido());
+
         return "vista/paciente";
     }
 }
